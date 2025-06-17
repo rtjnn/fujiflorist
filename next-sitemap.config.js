9553b47f7626332f21c983ed66238620061eaef5
Revicto <trigajulianrevicto@gmail.com>
2025-06-17 19:25:04 +0700

    Menambahkan sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://fujiflorist.online', // ganti dengan domainmu
+  generateRobotsTxt: true,               // akan membuat robots.txt
+  generateIndexSitemap: false,           // kalau kamu hanya pakai 1 sitemap
+  sitemapSize: 5000,                     // default
+};
